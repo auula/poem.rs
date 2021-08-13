b73ac95d485693b03847a5bd70bc4a6e96dbc6fa
Dings <deen.job@qq.com>
2021-08-13 20:02:13 +0800

    updated homepage.

---
+++ static/poem.js
@@ -0,0 +1,13 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/highlight.min.js"></script>
+
+<script src="https://cdn.bootcdn.net/ajax/libs/twitter-bootstrap/5.0.2/js/bootstrap.bundle.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.2.0/languages/rust.min.js"></script>
+
+hljs.initHighlightingOnLoad();
+			console.log("\t\t\t\t__________\n" +
+            "\t\t\t\t\\______   \\____   ____   _____  \n" +
+            "\t\t\t\t |     ___/  _ \\_/ __ \\ /     \\ \n" +
+            "\t\t\t\t |    |  (  <_> )  ___/|  Y Y  \\\n" +
+            "\t\t\t\t |____|   \\____/ \\___  >__|_|  /\n" +
+            "\t\t\t\t                     \\/      \\/ ");
+			console.log("If you find a bug on this page, please contact the developer <ding@ibyte.me>");
